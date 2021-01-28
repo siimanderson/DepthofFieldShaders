82bd6ab9d25b0fd1dc6575fc196955173d24b109
siimanderson <andersonsiim95@gmail.com>
2021-01-28 11:15:32 +0200

    AdaptiveBilaterDepthOfField base file

---
+++ AdaptiveBilateralDepthOfField.glsl
@@ -0,0 +1,27 @@
+sampler2D Color {
+    "addressU": "MIRROR",
+    "addressV": "MIRROR"
+};
+
+int Radius = 3 {
+    "min": 0,
+    "max": 20
+};
+
+int StandardDeviation1 = 1 {
+    "min" = 1,
+    "max" = 300
+};
+
+int StandardDeviation2 = 1 {
+    "min" = 1,
+    "max" = 300
+};
+
+out vec4 Output;
+
+##GLSL
+
+void main(){
+
+}
